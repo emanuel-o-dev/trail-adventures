dc46c380e3b64be8fc67259989d15329fa24b1f1
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-05-08 17:06:04 -0300

    feat: refactor TrailCardShort component to use forwardRef and improve layout; update trail mock data with localized names and metrics

--- components/TrailCardShort.tsx
+++ components/TrailCardShort.tsx
@@ -1,41 +1,42 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
-import { Divider } from "@rneui/base";
-import { Image } from "@rneui/themed";
+import { View, Text, Pressable, PressableProps } from "react-native";
+import React, { forwardRef } from "react";
+import { Divider } from "@rneui/themed";
+import { Image } from "@rneui/base";
 import { ITrailShort } from "../interfaces/ITrailShort.interface";
 
-export default function TrailCardShort(
-  trail: ITrailShort & { onPress: () => void },
-) {
-  return (
-    <Pressable
-      onPress={trail.onPress}
-      className="bg-zinc-800 rounded-lg p-4 m-2 gap-4 shadow-md justify-start"
-    >
-      <Image
-        source={trail.image || require("../assets/mountain.png")}
-        style={{ height: 200, width: "100%" }}
-        PlaceholderContent={<Text>Loading...</Text>}
-      />
-      <View>
-        <Text className="text-white text-lg font-bold">{trail.name}</Text>
-        <View className="flex-row gap-3 mt-2">
-          <Text className="text-gray-400"> {trail.difficulty}</Text>
-          <Divider orientation="vertical" width={1} color="#fff" />
-          <Text className="text-gray-400"> {trail.distance}</Text>
-        </View>
-        <View className="flex-row gap-3 mt-2">
-          <Text className="text-gray-400">{trail.type}</Text>
-          <Divider
-            orientation="vertical"
-            width={1}
-            className="h-1"
-            color="#fff"
-          />
+type TrailCardShortProps = PressableProps & {
+  trail: ITrailShort;
+};
 
-          <Text className="text-gray-400">{trail.time}</Text>
+const TrailCardShort = forwardRef<typeof Pressable, TrailCardShortProps>(
+  ({ trail, ...rest }, ref) => {
+    return (
+      <Pressable
+        ref={ref as React.Ref<React.ElementRef<typeof Pressable>>}
+        {...rest}
+        className="bg-zinc-800 rounded-lg p-4 m-2 gap-4 shadow-md justify-start w-full"
+      >
+        <Image
+          source={trail.image || require("../assets/mountain.png")}
+          style={{ height: 200, width: "100%" }}
+          PlaceholderContent={<Text>Loading...</Text>}
+        />
+        <View>
+          <Text className="text-white text-lg font-bold">{trail.name}</Text>
+          <View className="flex-row gap-3 mt-2">
+            <Text className="text-gray-100">{trail.difficulty}</Text>
+            <Divider color="white" className="w-3 self-center" />
+            <Text className="text-gray-300"> {trail.distance}</Text>
+          </View>
+          <View className="flex-row gap-3 mt-2">
+            <Text className="text-gray-100">{trail.type}</Text>
+            <Divider color="white" className="w-3 self-center" />
+            <Text className="text-gray-300">{trail.time}</Text>
+          </View>
         </View>
-      </View>
-    </Pressable>
-  );
-}
+      </Pressable>
+    );
+  },
+);
+
+export default TrailCardShort;
