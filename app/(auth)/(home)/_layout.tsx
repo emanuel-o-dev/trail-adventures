c64538726b827cddc53d4875a11ecc22bc1774e6
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-05-08 17:05:15 -0300

    feat: wrap tabs in SafeAreaProvider for improved layout handling

--- app/(auth)/(home)/_layout.tsx
+++ app/(auth)/(home)/_layout.tsx
@@ -1,17 +1,11 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function _layout() {
   return (
-    <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="home" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="details"
-          options={{ title: "Detalhes da trilha" }}
-        />
-      </Stack>
-    </SafeAreaProvider>
+    <Stack>
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="details" options={{ title: "Detalhes da trilha" }} />
+    </Stack>
   );
 }
