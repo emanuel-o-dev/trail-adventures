bb792389d7466b3856c1e7735ae82f989e1e4eba
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-05-25 22:53:27 -0300

    feat: implement user authentication and profile management with Zustand state management

--- app/(auth)/saved.tsx
+++ app/(auth)/saved.tsx
@@ -2,17 +2,18 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Button } from "@rneui/base";
 import TrailRepository from "../../src/database/TrailRepository";
+import UserRepository from "../../src/database/UserRepository";
 
 export default function saved() {
   const fecthData = async () => {
     console.log("test");
-    const repository = new TrailRepository();
+    const repository = new UserRepository();
     const trails = repository.all();
     console.log(trails);
   };
   const deleteData = async () => {
     console.log("delete");
-    const repository = new TrailRepository();
+    const repository = new UserRepository();
     const trails = repository.down();
     console.log(trails);
   };
---
+++ states/useUser.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { IUser } from "../interfaces/IUser.interface";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type State = {
+  user: IUser | null;
+};
+
+type Actions = {
+  reset: () => void;
+  setUser: (user: IUser) => void;
+  getUser: () => IUser | null;
+};
+
+const useUser = create<State & Actions>()(
+  persist(
+    (set, get) => ({
+      user: null,
+      setUser: (user: IUser) => {
+        set(() => ({
+          user: user,
+        }));
+      },
+      getUser: () => {
+        return get().user;
+      },
+      reset: () => {
+        set(() => ({
+          user: null,
+        }));
+      },
+    }),
+    {
+      name: "state-useUser",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
+
+export default useUser;
