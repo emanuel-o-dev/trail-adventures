4535c0fc6d01790b6714c2b874f5f58548d77498
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-04 03:50:36 -0300

    feat: mock functionality to fetch and delete saved trails

--- app/(auth)/path.tsx
+++ app/(auth)/path.tsx
@@ -2,6 +2,8 @@ import { View } from "react-native";
 import React from "react";
 import UserRepository from "../../src/database/UserRepository";
 import { Button } from "@rneui/base";
+import FavoriteRepository from "../../src/database/FavoritesRepository";
+import useUserStore from "../../states/useUser";
 
 export default function path() {
   const fecthData = async () => {
@@ -16,10 +18,29 @@ export default function path() {
     const trails = repository.down();
     console.log(trails);
   };
+  const fecthDataSaved = async () => {
+    const { getUser } = useUserStore();
+    const repository = new FavoriteRepository();
+    const userId = getUser()?.id.toString();
+    if (!userId) {
+      console.log("User not logged in. Cannot fetch saved trails.");
+      return null;
+    }
+    const savedTrails = repository.getSavedTrails(userId);
+    console.log(savedTrails);
+  };
+  const deleteDataSaved = async () => {
+    console.log("delete saved");
+    const repository = new FavoriteRepository();
+    const trails = repository.down();
+    console.log(trails);
+  };
   return (
     <View>
       <Button onPress={fecthData} title={"Test"} />
       <Button onPress={deleteData} title={"Delete"} />
+      <Button onPress={fecthDataSaved} title={"Saved"} />
+      <Button onPress={deleteDataSaved} title={"Delete Saved"} />
     </View>
   );
 }
