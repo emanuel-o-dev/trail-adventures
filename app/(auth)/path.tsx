5bba30a3d39adae8d4700844ae34136e0e75daad
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-03 23:05:37 -0300

    changed mock to controll db to (future) maps page

--- app/(auth)/path.tsx
+++ app/(auth)/path.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import UserRepository from "../../src/database/UserRepository";
+import { Button } from "@rneui/base";
 
 export default function path() {
+  const fecthData = async () => {
+    console.log("test");
+    const repository = new UserRepository();
+    const trails = repository.all();
+    console.log(trails);
+  };
+  const deleteData = async () => {
+    console.log("delete");
+    const repository = new UserRepository();
+    const trails = repository.down();
+    console.log(trails);
+  };
   return (
     <View>
-      <Text>path</Text>
+      <Button onPress={fecthData} title={"Test"} />
+      <Button onPress={deleteData} title={"Delete"} />
     </View>
   );
 }
