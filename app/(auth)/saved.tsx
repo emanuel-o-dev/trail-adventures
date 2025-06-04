c4115e7303b668dd377d620071573a3505ee6cd2
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-04 03:50:45 -0300

    refactor: restructure fetchSavedTrails function and update saved trails display logic

--- app/(auth)/saved.tsx
+++ app/(auth)/saved.tsx
@@ -1,27 +1,39 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import React from "react";
-import { Button } from "@rneui/base";
-import UserRepository from "../../src/database/UserRepository";
+import { Button, Text } from "@rneui/base";
 import FavoriteRepository from "../../src/database/FavoritesRepository";
 import useUserStore from "../../states/useUser";
+import TrailCardSaved from "../../components/TrailCardSaved";
 
 export default function saved() {
-  const { getUser } = useUserStore();
-  const userId = getUser()?.id.toString();
-  const fetchData = async () => {
-    console.log("Fetching saved trails...");
+  function fetchSavedTrails() {
+    const { getUser } = useUserStore();
     const repository = new FavoriteRepository();
+    const userId = getUser()?.id.toString();
     if (!userId) {
-      console.log("User not logged in. Cannot fetch saved trails.");
-      return;
+      console.log("Usuario não logado. Não é possível buscar trilhas salvas.");
+      return null;
     }
     const savedTrails = repository.getSavedTrails(userId);
-    console.log(savedTrails);
-  };
+    return savedTrails;
+  }
+  const savedTrails = fetchSavedTrails();
+  if (!savedTrails || savedTrails.length === 0) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Text h1 />
+        <Text h4>Você não tem trilhas salvas</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
-      <Button onPress={fetchData} title={"Fetch Saved Trails"} />
+      <FlatList
+        data={savedTrails}
+        keyExtractor={(item) => item.dateVisited?.toString() || ""}
+        renderItem={({ item }) => <TrailCardSaved trail={item} />}
+      ></FlatList>
     </View>
   );
 }
