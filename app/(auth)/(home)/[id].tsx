acede1c87587715cddd8b33e6b5ff609175fe647
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-04 23:33:22 -0300

    feat: create TrailScreen component to display trail details using dynamic routing

--- app/(auth)/(home)/[id].tsx
+++ app/(auth)/(home)/[id].tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TrailCardFull from "../../../components/TrailCardFull";
 import { useGlobalSearchParams } from "expo-router";
 
-export default function details() {
+export default function TrailScreen() {
   const { id } = useGlobalSearchParams();
 
   return <TrailCardFull id={Number(id)} />;
