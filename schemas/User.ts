61e23d3755b7a4bd41d9292fd7bff94917b470e5
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-25 18:24:29 -0300

    Update User schema: add custom error message for password validation; refactor useUser state management to use User type instead of IUser.

--- schemas/User.ts
+++ schemas/User.ts
@@ -3,7 +3,7 @@ export const User = z.object({
   id: z.number(),
   name: z.string(),
   email: z.string().email("Deve ser um endereço de email válido"),
-  password: z.string().min(6),
+  password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
   createdAt: z
     .date()
     .default(() => new Date())
