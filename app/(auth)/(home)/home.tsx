409bd64f8c4f1359de2adb8f64ae34a24fea67e4
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-06-26 20:52:47 -0300

    feat: enhance home screen with trail and marker loading functionality

--- app/(auth)/(home)/home.tsx
+++ app/(auth)/(home)/home.tsx
@@ -2,14 +2,34 @@ import React from "react";
 import useNavigationExitOnBack from "../../../hooks/useNavigationExitOnBack";
 import Maps from "../../../components/Maps";
 import TrailWrapper from "../../../components/TrailWrapper";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import useTrails from "../../../states/useTrails";
+import { useAsyncData } from "../../../hooks/useAsyncData";
+import { TrailShort } from "../../../schemas/TrailShort";
+
 export default function _screen() {
   useNavigationExitOnBack();
 
+  const { trails, loadTrails, markers, loadMarkers } = useTrails();
+  const { loading } = useAsyncData<{ trails: TrailShort[] }>(async () => {
+    loadTrails();
+    loadMarkers();
+    return { trails };
+  });
+
+  if (loading) return loading;
+
   return (
-    <View className="flex-1">
-      <Maps />
-      <TrailWrapper />
+    <View style={styles.container}>
+      <Maps markers={markers} />
+      <TrailWrapper trails={trails} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+});
